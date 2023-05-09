0f193ae214c1f05a330da48036059675b58b397a
Camila Machado <sra.mac18@gmail.com>
2023-05-09 19:15:50 -0300

    Project Created

---
+++ src/main/java/com/devsuperior/DList/DListApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/DList/DListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
